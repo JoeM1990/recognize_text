7558623f1e43018e696bab4e9c344f6ef9a69c8e
Joe Monkila <jnkiwa25@gmail.com>
2024-07-16 10:09:38 +0100

    bottom text, top text and login content

---
+++ lib/widgets/top_text.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'login_content.dart';
+
+class TopText extends StatefulWidget {
+  const TopText({Key? key}) : super(key: key);
+
+  @override
+  State<TopText> createState() => _TopTextState();
+}
+
+class _TopTextState extends State<TopText> {
+  @override
+  void initState() {
+    ChangeScreenAnimation.topTextAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {});
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HelperFunctions.wrapWithAnimatedBuilder(
+      animation: ChangeScreenAnimation.topTextAnimation,
+      child: Text(
+        ChangeScreenAnimation.currentScreen == Screens.createAccount
+            ? 'Create\nAccount'
+            : 'Welcome\nBack',
+        style: const TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
