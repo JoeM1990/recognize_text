60406cafa26e52917acdc778d4131df738893e48
Joe Monkila <jnkiwa25@gmail.com>
2024-07-19 15:10:46 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:recognize_text/views/SplashScreen.dart';
+import 'package:recognize_text/views/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //title: '',
-
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
